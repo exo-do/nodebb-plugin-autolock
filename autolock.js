e74632c935010725ea5ac400b422da408ce2f2b8
hawkings <daniel.marin.94@gmail.com>
2015-09-22 03:13:59 -0400

    bugs fixed

--- autolock.js
+++ autolock.js
@@ -6,14 +6,10 @@ module.exports.autolock = function (data, callback) {
 	Topics.getPostCount(data.tid, function (err, postCount) {
 		if(err) {
 			callback(err);
-		} if(postCount >= 2) {
-			Topics.setTopicField(data.tid, 1, callback);
+		} if(postCount >= 1000) {
+			Topics.setTopicField(data.tid, 'locked', 1, callback);
 		} else {
 			callback();
 		}
-	})
-	//Topics.tools.lock(tid, uid (1), callback);
-	//Topics.getPostCount(tid, callback);
-	//Topics.setTopicField(tid, 'locked', lock ? 1 : 0, next);
-	//db.setObjectField('topic:' + tid, field, value, callback);
+	});
 }
